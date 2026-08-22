3b33a00f68d5c079466d3f973f8b2f4a7b4518e3
Colby Brooks <167918852+cbrookscode@users.noreply.github.com>
2026-08-22 11:20:42 -0400

    tree viewer seperated into its own user control now

--- JournalApp/Views/AddFolderWindow.xaml.cs
+++ JournalApp/Views/AddFolderWindow.xaml.cs
@@ -26,18 +26,11 @@ namespace JournalApp.Views
         public JournalFolder CreatedFolder { get; set; } = new JournalFolder();
 
         private JournalDatabase _journalDatabase;
-        private ObservableCollection<JournalTreeItem> _folderTree;
-        private Dictionary<int, JournalFolder> _folderDict;
         public AddFolderWindow(JournalDatabase journalDatabase)
         {
             InitializeComponent();
             _journalDatabase = journalDatabase;
-            List<JournalTreeItem> treeInput = new List<JournalTreeItem>();
-            treeInput.AddRange(_journalDatabase.GetAllEntries());
-            treeInput.AddRange(_journalDatabase.GetAllFolders());
-
-            (_folderTree, _folderDict) = _journalDatabase.SetupChildrenFolders(treeInput);
-            FolderView.ItemsSource = _folderTree;
+            FolderTree.Initialize(journalDatabase);
         }
 
         private void Submit_Click(object sender, RoutedEventArgs e)
@@ -51,55 +44,11 @@ namespace JournalApp.Views
             CreatedFolder.Name = FolderName.Text;
             CreatedFolder.ParentFolderId = null;
 
-            if (FolderView.SelectedItem is JournalFolder selectedFolder)
+            if (FolderTree.SelectedItem is JournalFolder selectedFolder)
             {
                 CreatedFolder.ParentFolderId = selectedFolder.Id;
             }
             DialogResult = true;
-        }
-
-        public void Delete_Click(object sender, RoutedEventArgs e)
-        {
-            Button button = (Button)sender;
-
-            JournalTreeItem treeItem = (JournalTreeItem)button.Tag;
-
-            DeleteConfirmationWindow confirmationWindow = new DeleteConfirmationWindow("You are about to delete the following folder:", treeItem.Name);
-            bool? result = confirmationWindow.ShowDialog();
-            if (result == true)
-            {
-                if (treeItem is JournalEntry entry)
-                {
-                    _journalDatabase.DeleteEntry(entry.Id);
-                    if (entry.FolderId != null)
-                    {
-                        if (_folderDict.TryGetValue(entry.FolderId.Value, out JournalFolder? parent))
-                        {
-                            if (parent != null)
-                            {
-                                parent.Children.Remove(entry);
-                            }
-                        }
-                    }
-                } else if (treeItem is JournalFolder folder)
-                {
-                    _journalDatabase.DeleteFolder(folder.Id);
-                    if (folder.ParentFolderId != null)
-                    {
-                        if (_folderDict.TryGetValue(folder.ParentFolderId.Value, out JournalFolder? parent))
-                        {
-                            if (parent != null)
-                            {
-                                parent.Children.Remove(folder);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        _folderTree.Remove(folder);
-                    }
-                }
-            }
         }
     }
 }
---
+++ JournalApp/Views/FolderTreeView.xaml.cs
@@ -0,0 +1,100 @@
+﻿using JournalApp.Data;
+using JournalApp.Models;
+using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JournalApp.Views
+{
+    /// <summary>
+    /// Interaction logic for FolderTreeView.xaml
+    /// </summary>
+    public partial class FolderTreeView : UserControl
+    {
+        public IEnumerable? ItemsSource
+        {
+            get => FolderView.ItemsSource;
+            set => FolderView.ItemsSource = value;
+        }
+
+        public JournalTreeItem? SelectedItem
+        {
+            get => FolderView.SelectedItem as JournalTreeItem;
+        }
+        private JournalDatabase _journalDatabase;
+        private ObservableCollection<JournalTreeItem> _folderTree;
+        private Dictionary<int, JournalFolder> _folderDict;
+
+        public FolderTreeView()
+        {
+            InitializeComponent();
+        }
+        
+        public void Initialize(JournalDatabase journalDatabase)
+        {
+            _journalDatabase = journalDatabase;
+            List<JournalTreeItem> treeInput = new List<JournalTreeItem>();
+            treeInput.AddRange(_journalDatabase.GetAllEntries());
+            treeInput.AddRange(_journalDatabase.GetAllFolders());
+
+            (_folderTree, _folderDict) = _journalDatabase.SetupChildrenFolders(treeInput);
+            FolderView.ItemsSource = _folderTree;
+        }
+
+        public void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            JournalTreeItem treeItem = (JournalTreeItem)button.Tag;
+
+            DeleteConfirmationWindow confirmationWindow = new DeleteConfirmationWindow("You are about to delete the following folder:", treeItem.Name);
+            bool? result = confirmationWindow.ShowDialog();
+            if (result == true)
+            {
+                if (treeItem is JournalEntry entry)
+                {
+                    _journalDatabase.DeleteEntry(entry.Id);
+                    if (entry.FolderId != null)
+                    {
+                        if (_folderDict.TryGetValue(entry.FolderId.Value, out JournalFolder? parent))
+                        {
+                            if (parent != null)
+                            {
+                                parent.Children.Remove(entry);
+                            }
+                        }
+                    }
+                }
+                else if (treeItem is JournalFolder folder)
+                {
+                    _journalDatabase.DeleteFolder(folder.Id);
+                    if (folder.ParentFolderId != null)
+                    {
+                        if (_folderDict.TryGetValue(folder.ParentFolderId.Value, out JournalFolder? parent))
+                        {
+                            if (parent != null)
+                            {
+                                parent.Children.Remove(folder);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        _folderTree.Remove(folder);
+                    }
+                }
+            }
+        }
+    }
+}
